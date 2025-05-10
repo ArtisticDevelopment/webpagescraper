d0ce9db5f603090b30f08d900e17b352b6d521c1
Silas Green <scgreen160@gmail.com>
2025-05-10 16:11:44 -0400

    updates

--- scrape.py
+++ scrape.py
@@ -1,6 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
+#gathers website html in text form as soup 
 res = requests.get('https://news.ycombinator.com/')
 soup = BeautifulSoup(res.text, "html.parser")
-print(soup.find(id='score_43913705'))
+#collects each link that is a child of a titleline span
+links = soup.select('.titleline > a')
+#collects each subtext below the title/link
+subtext = soup.select('.subtext')
+
+def create_custom_hn(links, subtext):
+  hacker_news_list = []
+  for index, item in enumerate(links):
+    title = links[index].getText()
+    href = links[index].get('href', None)
+    votesList = subtext[index].select('.score')
+    if votesList:
+      points = int(votesList[0].getText().replace(' points', ''))
+      if points > 100:
+        print(points)
+        hacker_news_list.append({'Title: ': title, 'Link: ': href})
+  
+  return hacker_news_list
+
+(create_custom_hn(links, subtext))
